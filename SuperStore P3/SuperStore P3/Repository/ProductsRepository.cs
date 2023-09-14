ae7fbe5164cb66430b0eb00742e28a28537c9cf4
Refilwe Mmutlwane <refilwemmutlwane@gmail.com>
2023-09-14 20:49:52 +0200

    GetAll method and Index method modified

--- SuperStore P3/SuperStore P3/Repository/ProductsRepository.cs
+++ SuperStore P3/SuperStore P3/Repository/ProductsRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace EcoPower_Logistics.Repository
+﻿using Data;
+using Models;
+
+namespace EcoPower_Logistics.Repository
 {
     public class ProductsRepository
     {
+        protected readonly SuperStoreContext _context = new SuperStoreContext();
+
+            // GET ALL: Products
+            public IEnumerable<Product> GetAll()
+            {
+                return _context.Products.ToList();
+            }
+
+        }
     }
-}
